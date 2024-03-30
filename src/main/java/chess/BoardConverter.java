f9d8ef428f9be820d3f2ca37a9b0a28e38f361b3
mzeong <mjeong@sookmyung.ac.kr>
2024-03-30 23:52:09 +0900

    feat: 보드와 문자열 상호 변환 로직 생성

---
+++ src/main/java/chess/BoardConverter.java
@@ -0,0 +1,101 @@
+package chess;
+
+import chess.domain.piece.Bishop;
+import chess.domain.piece.Empty;
+import chess.domain.piece.King;
+import chess.domain.piece.Knight;
+import chess.domain.piece.Pawn;
+import chess.domain.piece.Piece;
+import chess.domain.piece.Queen;
+import chess.domain.piece.Rook;
+import chess.domain.piece.Side;
+import chess.domain.position.Position;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public enum BoardConverter {
+
+    BLACK_ROOK(() -> new Rook(Side.BLACK), "R"),
+    WHITE_ROOK(() -> new Rook(Side.WHITE), "r"),
+    BLACK_KNIGHT(() -> new Knight(Side.BLACK), "N"),
+    WHITE_KNIGHT(() -> new Knight(Side.WHITE), "n"),
+    BLACK_BISHOP(() -> new Bishop(Side.BLACK), "B"),
+    WHITE_BISHOP(() -> new Bishop(Side.WHITE), "b"),
+    BLACK_QUEEN(() -> new Queen(Side.BLACK), "Q"),
+    WHITE_QUEEN(() -> new Queen(Side.WHITE), "q"),
+    BLACK_KING(() -> new King(Side.BLACK), "K"),
+    WHITE_KING(() -> new King(Side.WHITE), "k"),
+    BLACK_PAWN(() -> new Pawn(Side.BLACK), "P"),
+    WHITE_PAWN(() -> new Pawn(Side.WHITE), "p"),
+    EMPTY(Empty::instance, "."),
+    ;
+
+    private final Supplier<Piece> toPiece;
+    private final String string;
+
+    BoardConverter(Supplier<Piece> toPiece, String string) {
+        this.toPiece = toPiece;
+        this.string = string;
+    }
+
+    private Piece piece() {
+        return toPiece.get();
+    }
+
+    private static String toString(Piece piece) {
+        return Arrays.stream(values())
+                .filter(it -> it.piece().equals(piece))
+                .findAny()
+                .map(it -> it.string)
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 Piece"));
+    }
+
+    private static Piece toObject(String string) {
+        return Arrays.stream(values())
+                .filter(it -> it.string.equals(string))
+                .findAny()
+                .map(BoardConverter::piece)
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 String"));
+    }
+
+    public static String convertToString(List<Piece> object) {
+        return object.stream()
+                .map(BoardConverter::toString)
+                .collect(Collectors.joining(""));
+    }
+
+    public static Map<Position, Piece> convertToBoard(String string) {
+        List<Piece> pieces = convertToPieces(string);
+        List<Position> positions = Position.allPositions();
+
+        validateSameSize(pieces, positions);
+
+        return IntStream.range(0, positions.size())
+                .boxed()
+                .collect(Collectors.toMap(
+                        positions::get,
+                        pieces::get,
+                        (oldValue, newValue) -> newValue,
+                        LinkedHashMap::new
+                ));
+    }
+
+    private static List<Piece> convertToPieces(String string) {
+        String[] split = string.split("");
+
+        return Arrays.stream(split)
+                .map(BoardConverter::toObject)
+                .toList();
+    }
+
+    private static void validateSameSize(List<Piece> pieces, List<Position> positions) {
+        if (pieces.size() != positions.size()) {
+            throw new IllegalArgumentException("유효하지 않은 Pieces");
+        }
+    }
+}
---
+++ src/test/java/chess/BoardConverterTest.java
@@ -0,0 +1,36 @@
+package chess;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import chess.domain.game.Game;
+import chess.domain.game.InitialGameCreator;
+import chess.domain.piece.Piece;
+import chess.domain.position.Position;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BoardConverterTest {
+
+    @DisplayName("보드를 문자열로 변환한다.")
+    @Test
+    void convertToString() {
+        Game game = new Game(new InitialGameCreator());
+
+        String actual = BoardConverter.convertToString(game.board());
+        String expected = "RNBQKBNRPPPPPPPP................................pppppppprnbqkbnr";
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("문자열을 보드로 변환한다.")
+    @Test
+    void convertToBoard() {
+        String string = "RNBQKBNRPPPPPPPP................................pppppppprnbqkbnr";
+
+        Map<Position, Piece> actual = BoardConverter.convertToBoard(string);
+        Map<Position, Piece> expected = new InitialGameCreator().createBoard();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
