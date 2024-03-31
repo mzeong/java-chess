742dc07b9039351ae7e60cd0a07f77240de04ae3
mzeong <mjeong@sookmyung.ac.kr>
2024-03-31 23:17:05 +0900

    refactor: PiecesTest에 있던 테스트 이동 및 누락된 Pawn 추가

---
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -0,0 +1,44 @@
+package chess.domain.piece;
+
+import chess.domain.position.Position;
+import chess.domain.route.Route;
+import java.util.List;
+
+public abstract class Pawn extends Piece {
+
+    public Pawn(Side side) {
+        super(side);
+    }
+
+    abstract boolean isAttackable(Position source, Position target, Route route);
+
+    abstract boolean isTwoForwardFromInitialPosition(Position source, Position target);
+
+    abstract boolean isOneForward(Position source, Position target);
+
+    @Override
+    boolean hasFollowedRule(Position source, Position target, Route route) {
+        return isAttackable(source, target, route) ||
+                isNotAttack(route) && (isTwoForwardFromInitialPosition(source, target) || isOneForward(source, target));
+    }
+
+    private boolean isNotAttack(Route route) {
+        return route.isTargetPieceEmpty();
+    }
+
+    @Override
+    public double score(List<Piece> pieces) {
+        int sameSidePawnCount = countSameSidePawn(pieces);
+
+        if (sameSidePawnCount > 1) {
+            return 0.5;
+        }
+        return 1;
+    }
+
+    private int countSameSidePawn(List<Piece> pieces) {
+        return (int) pieces.stream()
+                .filter(this::equals)
+                .count();
+    }
+}
---
+++ src/test/java/chess/domain/route/RouteTest.java
@@ -0,0 +1,32 @@
+package chess.domain.route;
+
+import static chess.fixture.PieceFixture.BLACK_ROOK;
+import static chess.fixture.PieceFixture.EMPTY;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RouteTest {
+
+    @DisplayName("경로에 존재하는 모든 기물이 EMPTY는 아니다.")
+    @Test
+    void notAllPathPiecesEmpty() {
+        Route route = new Route(List.of(EMPTY, BLACK_ROOK), EMPTY);
+
+        boolean actual = route.notAllPathPiecesEmpty();
+
+        assertThat(actual).isTrue();
+    }
+
+    @DisplayName("경로에 존재하는 모든 기물이 EMPTY이다.")
+    @Test
+    void allPathPiecesEmpty() {
+        Route route = new Route(List.of(EMPTY, EMPTY), EMPTY);
+
+        boolean actual = route.notAllPathPiecesEmpty();
+
+        assertThat(actual).isFalse();
+    }
+}
