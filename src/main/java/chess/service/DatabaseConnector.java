a8fd2681cad26935ef00fc91356d666a65e40248
mzeong <mjeong@sookmyung.ac.kr>
2024-04-02 13:56:46 +0900

    refactor: 커넥션 획득 실패 시 null 리턴 코드 수정

--- src/main/java/chess/service/DatabaseConnector.java
+++ src/main/java/chess/service/DatabaseConnector.java
@@ -16,9 +16,7 @@ public class DatabaseConnector {
         try {
             return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
         } catch (final SQLException e) {
-            System.err.println("DB 연결 오류:" + e.getMessage());
-            e.printStackTrace();
-            return null;
+            throw new RuntimeException("DB 연결 오류: " + e.getMessage());
         }
     }
 }
